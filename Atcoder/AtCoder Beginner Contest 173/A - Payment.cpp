fa5d568addd827116e6c14bae4a4c5cc1f0df940
Ankit Singh Dungriyal <36338087+ankit-singh-dungriyal@users.noreply.github.com>
2020-07-06 17:29:25 +0530

    Create A - Payment.cpp

---
+++ Atcoder/AtCoder Beginner Contest 173/A - Payment.cpp
@@ -0,0 +1,29 @@
+// 'By Anki'
+ 
+#include<bits/stdc++.h>
+// #include<iostream>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define pb push_back
+#define mp make_pair
+#define wr(i) cout<<#i<<" = "<<i<<",  ";
+#define wre(i) cout<<#i<<"  =  "<<i<<endl;
+#define all(v) v.begin(),v.end()
+typedef long long ll;
+#define lim 1000000007LL
+typedef unsigned long long ull;
+#define M 10004
+using u64= uint64_t;
+ 
+int main()
+{
+fast
+ll t=1LL;
+// cin>>t;
+while(t--)
+{
+   ll n;
+   cin>>n;
+   cout<<(1000-n%1000)%1000;
+}
+} 
