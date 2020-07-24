5677e5f4c5064c05405ed3262d241f34a856c446
Ankit Singh Dungriyal <36338087+ankit-singh-dungriyal@users.noreply.github.com>
2020-07-24 13:49:38 +0530

    Create Cutting a material.cpp

---
+++ Hackerearth/July Circuits 2020/Cutting a material.cpp
@@ -0,0 +1,31 @@
+// Concept used: Basic maths
+// 'By Anki'
+ 
+#include<bits/stdc++.h>
+// #include<iostream>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define pb push_back
+#define mp make_pair
+#define wr(i) cout<<#i<<" = "<<i<<",  ";
+#define wre(i) cout<<#i<<"  =  "<<i<<endl;
+#define all(v) v.begin(),v.end()
+typedef long long ll;
+#define lim 1000000007LL
+typedef unsigned long long ull;
+#define M 200005;
+using u64= uint64_t;
+
+
+int main()
+{
+fast
+ll t=1LL;
+cin>>t;
+while(t--)
+{
+ ll n;
+ cin>>n;
+ cout<<(n*n+n+2)/2<<"\n";
+}
+} 
