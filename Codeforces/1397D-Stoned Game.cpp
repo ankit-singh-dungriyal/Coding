3b67dfc5cd4cffc3a51e7540d4011c65d3bf1f1c
Ankit Singh Dungriyal <36338087+ankit-singh-dungriyal@users.noreply.github.com>
2020-08-31 12:10:27 +0530

    Create 1397D-Stoned Game.cpp

---
+++ Codeforces/1397D-Stoned Game.cpp
@@ -0,0 +1,46 @@
+// By 'Anki'
+// Concept used: Game Theory
+
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define pb push_back
+#define mp make_pair
+#define wr(i) cout<<#i<<" = "<<i<<",  ";
+#define wre(i) cout<<#i<<"  =  "<<i<<endl;
+#define all(v) v.begin(),v.end()
+typedef long long ll;
+#define lim 1000000000000000LL
+typedef unsigned long long ull;
+#define M 200005LL
+#define F first
+#define S second
+using u64= uint64_t;
+
+
+
+int main()
+{
+fast
+ll t=1LL;
+cin>>t;
+
+while(t--)
+{
+ll n;
+cin>>n;
+vector<ll>v(n);
+for(auto &x:v)cin>>x;
+ll ans=0LL;
+sort(all(v));
+for(int i=0;i<n;i++)ans+=v[i];
+if(ans-v[n-1]<v[n-1])cout<<"T\n";
+else{
+    if(ans&1)cout<<"T\n";
+    else cout<<"HL\n";
+}
+
+
+
+}
+} 
