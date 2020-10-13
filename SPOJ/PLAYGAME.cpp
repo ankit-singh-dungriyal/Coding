307e412bddf6f0725918a09fb93c3164a5c6a93a
Ankit Singh Dungriyal <36338087+ankit-singh-dungriyal@users.noreply.github.com>
2020-10-13 15:10:13 +0530

    Create PLAYGAME.cpp

---
+++ SPOJ/PLAYGAME.cpp
@@ -0,0 +1,48 @@
+// By 'Anki'
+
+// Concept used: Game Theory
+
+#include<bits/stdc++.h>
+// #include<thread>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define pb push_back
+#define mp make_pair
+#define wr(i) cout<<#i<<" = "<<i<<",  ";
+#define wre(i) cout<<#i<<"  =  "<<i<<endl;
+#define all(v) v.begin(),v.end()
+typedef long long ll;
+#define lim 1000000000000000000LL
+typedef unsigned long long ull;
+#define mod 1000000007
+#define N 30
+#define F first
+#define S second
+using u64= uint64_t;
+
+
+void solve(){
+  ll n;
+  cin>>n;
+  if(n%3==0){
+      cout<<"Ragini\n";
+  }
+  else cout<<"Hemlata\n";
+
+}
+
+int main()
+{
+fast
+#ifndef ONLINE_JUDGE
+freopen("input.txt","r",stdin);
+freopen("output.txt","w",stdout);
+#endif
+ll t=1LL;
+cin>>t;
+while(t--)
+{
+solve();   
+}
+ 
+} 
