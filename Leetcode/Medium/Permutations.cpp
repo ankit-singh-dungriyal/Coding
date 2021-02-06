00a916a3d0b4c8548140a85acc884db745681e43
Ankit Singh Dungriyal <36338087+ankit-singh-dungriyal@users.noreply.github.com>
2021-02-06 12:04:10 +0530

    Create Permutations.cpp

---
+++ Leetcode/Medium/Permutations.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>>ans;
+        sort(nums.begin(),nums.end());
+        ans.push_back(nums);
+        while(next_permutation(nums.begin(),nums.end()))ans.push_back(nums);
+        return ans;
+    }
+};
