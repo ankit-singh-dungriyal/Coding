6fdbbeaab2e7b50eb15bd2e1bfebf95529986a8f
Ankit Singh Dungriyal <36338087+ankit-singh-dungriyal@users.noreply.github.com>
2021-02-23 22:17:11 +0530

    A_Wizard of Orz

---
+++ Codeforces/1467 - A_Wizard of Orz.cpp
@@ -0,0 +1,62 @@
+/*
+Contest Id: 1467
+Submission Id: 108166364
+Date & Time: 22/02/2021 15:34:39
+Tags/Concept used: ['constructive algorithms', 'greedy', 'math']
+*/
+
+
+#include<bits/stdc++.h>
+// #include<thread>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define pb push_back
+#define mp make_pair
+#define wr(i) cout<<#i<<" = "<<i<<",  ";
+#define wre(i) cout<<#i<<"  =  "<<i<<endl;
+#define all(v) v.begin(),v.end()
+typedef long long ll;
+#define lim 1000000000000000000LL
+typedef unsigned long long ull;
+#define mod 1000000007
+#define M 31
+#define N 30
+#define F first
+#define S second
+using u64= uint64_t;
+
+
+
+void solve(){
+    ll n;
+    cin>>n;
+    if(n==1)cout<<"9\n";
+    else{
+        string ans="98";
+        int k=9;
+        for(int i=3;i<=n;i++){
+            ans+='0'+k;
+            k=(k+1)%10;
+        }
+        cout<<ans<<"\n";
+    }
+
+    return;
+}
+
+int main()
+{
+fast
+#ifndef ONLINE_JUDGE
+freopen("input.txt","r",stdin);
+freopen("output.txt","w",stdout);
+#endif
+ll t=1LL;
+cin>>t;
+while(t--)
+{
+solve();  
+
+}
+ 
+} 
