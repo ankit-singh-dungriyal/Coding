a6e9089a3d27d6b4dacdb963400b85a48f8f0e32
Ankit Singh Dungriyal <36338087+ankit-singh-dungriyal@users.noreply.github.com>
2021-02-23 22:17:26 +0530

    B_New Year's Number

---
+++ Codeforces/1475 - B_New Year's Number.cpp
@@ -0,0 +1,55 @@
+/*
+Contest Id: 1475
+Submission Id: 108016717
+Date & Time: 20/02/2021 15:30:30
+Tags/Concept used: ['brute force', 'dp', 'math']
+*/
+
+
+#include<bits/stdc++.h>
+// #include<thread>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define pb push_back
+#define mp make_pair
+#define wr(i) cout<<#i<<" = "<<i<<",  ";
+#define wre(i) cout<<#i<<"  =  "<<i<<endl;
+#define all(v) v.begin(),v.end()
+typedef long long ll;
+#define lim 1000000000000000000LL
+typedef unsigned long long ull;
+#define mod 1000000007
+#define M 31
+#define N 30
+#define F first
+#define S second
+using u64= uint64_t;
+
+
+
+void solve(){
+    ll n;
+    cin>>n;
+    ll k=n/2020;
+    ll z=n%2020;
+    if(z<=k)cout<<"YES\n";
+    else cout<<"NO\n";
+    return;
+}
+
+int main()
+{
+fast
+#ifndef ONLINE_JUDGE
+freopen("input.txt","r",stdin);
+freopen("output.txt","w",stdout);
+#endif
+ll t=1LL;
+cin>>t;
+while(t--)
+{
+solve();  
+
+}
+ 
+} 
