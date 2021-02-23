92356a6d95b1a08145fbabb0ef6690349f7c623d
Ankit Singh Dungriyal <36338087+ankit-singh-dungriyal@users.noreply.github.com>
2021-02-23 22:17:29 +0530

    A_Odd Divisor

---
+++ Codeforces/1475 - A_Odd Divisor.cpp
@@ -0,0 +1,54 @@
+/*
+Contest Id: 1475
+Submission Id: 108016512
+Date & Time: 20/02/2021 15:27:57
+Tags/Concept used: ['math', 'number theory']
+*/
+
+
+#include<bits/stdc++.h>
+// #include<thread>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define pb push_back
+#define mp make_pair
+#define wr(i) cout<<#i<<" = "<<i<<",  ";
+#define wre(i) cout<<#i<<"  =  "<<i<<endl;
+#define all(v) v.begin(),v.end()
+typedef long long ll;
+#define lim 1000000000000000000LL
+typedef unsigned long long ull;
+#define mod 1000000007
+#define M 31
+#define N 30
+#define F first
+#define S second
+using u64= uint64_t;
+
+  bool isptwo(ll x) 
+    {return (x&&!(x&(x-1)));}
+
+void solve(){
+    ll n;
+    cin>>n;
+    if(isptwo(n))cout<<"NO\n";
+    else cout<<"YES\n";
+    return;
+}
+
+int main()
+{
+fast
+#ifndef ONLINE_JUDGE
+freopen("input.txt","r",stdin);
+freopen("output.txt","w",stdout);
+#endif
+ll t=1LL;
+cin>>t;
+while(t--)
+{
+solve();  
+
+}
+ 
+} 
