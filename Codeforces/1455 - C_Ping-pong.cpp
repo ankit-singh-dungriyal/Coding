9ad3b4da96e14b818ddc415a8d45cb77527382c5
Ankit Singh Dungriyal <36338087+ankit-singh-dungriyal@users.noreply.github.com>
2021-02-23 22:17:48 +0530

    C_Ping-pong

---
+++ Codeforces/1455 - C_Ping-pong.cpp
@@ -0,0 +1,50 @@
+/*
+Contest Id: 1455
+Submission Id: 106665932
+Date & Time: 06/02/2021 15:47:58
+Tags/Concept used: ['constructive algorithms', 'games', 'math']
+*/
+
+
+#include<bits/stdc++.h>
+// #include<thread>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define pb push_back
+#define mp make_pair
+#define wr(i) cout<<#i<<" = "<<i<<",  ";
+#define wre(i) cout<<#i<<"  =  "<<i<<endl;
+#define all(v) v.begin(),v.end()
+typedef long long ll;
+#define lim 1000000000000000000LL
+typedef unsigned long long ull;
+#define mod 1000000007
+#define M 31
+#define N 30
+#define F first
+#define S second
+using u64= uint64_t;
+
+void solve(){
+    
+    ll x,y;
+    cin>>x>>y;
+    cout<<x-1<<" "<<y<<"\n";
+    return;
+}
+
+int main()
+{
+fast
+#ifndef ONLINE_JUDGE
+freopen("input.txt","r",stdin);
+freopen("output.txt","w",stdout);
+#endif
+ll t=1LL;
+cin>>t;
+while(t--)
+{
+solve();   
+}
+ 
+} 
