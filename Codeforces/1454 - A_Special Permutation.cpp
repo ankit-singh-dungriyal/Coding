755a1c690cb501055e4409e6708f411709b7afb2
Ankit Singh Dungriyal <36338087+ankit-singh-dungriyal@users.noreply.github.com>
2021-02-23 22:18:05 +0530

    A_Special Permutation

---
+++ Codeforces/1454 - A_Special Permutation.cpp
@@ -0,0 +1,50 @@
+/*
+Contest Id: 1454
+Submission Id: 106520614
+Date & Time: 05/02/2021 15:23:53
+Tags/Concept used: ['constructive algorithms', 'probabilities']
+*/
+
+
+#include<bits/stdc++.h>
+// #include<thread>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define pb push_back
+#define mp make_pair
+#define wr(i) cout<<#i<<" = "<<i<<",  ";
+#define wre(i) cout<<#i<<"  =  "<<i<<endl;
+#define all(v) v.begin(),v.end()
+typedef long long ll;
+#define lim 1000000000000000000LL
+typedef unsigned long long ull;
+#define mod 1000000007
+#define M 31
+#define N 30
+#define F first
+#define S second
+using u64= uint64_t;
+
+void solve(){
+    ll n;
+    cin>>n;
+    for(int i=0;i<n-1;i++)cout<<i+2<<" ";
+    cout<<1<<"\n";
+    return;
+}
+
+int main()
+{
+fast
+#ifndef ONLINE_JUDGE
+freopen("input.txt","r",stdin);
+freopen("output.txt","w",stdout);
+#endif
+ll t=1LL;
+cin>>t;
+while(t--)
+{
+solve();   
+}
+ 
+} 
