c2a86c99724500222917dab4e0b90e655bc36675
Ankit Singh Dungriyal <36338087+ankit-singh-dungriyal@users.noreply.github.com>
2021-02-23 22:18:20 +0530

    A_Robot Program

---
+++ Codeforces/1452 - A_Robot Program.cpp
@@ -0,0 +1,52 @@
+/*
+Contest Id: 1452
+Submission Id: 106356204
+Date & Time: 03/02/2021 15:33:09
+Tags/Concept used: ['math']
+*/
+
+
+#include<bits/stdc++.h>
+// #include<thread>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define pb push_back
+#define mp make_pair
+#define wr(i) cout<<#i<<" = "<<i<<",  ";
+#define wre(i) cout<<#i<<"  =  "<<i<<endl;
+#define all(v) v.begin(),v.end()
+typedef long long ll;
+#define lim 1000000000000000000LL
+typedef unsigned long long ull;
+#define mod 1000000007
+#define M 31
+#define N 30
+#define F first
+#define S second
+using u64= uint64_t;
+
+void solve(){
+    ll x,y;
+    cin>>x>>y;
+    ll tmp=abs(x-y);
+    ll ans=max(0LL,2*tmp-1)+2*min(x,y);
+    cout<<max(0LL,ans)<<"\n";
+
+    return;
+}
+
+int main()
+{
+fast
+#ifndef ONLINE_JUDGE
+freopen("input.txt","r",stdin);
+freopen("output.txt","w",stdout);
+#endif
+ll t=1LL;
+cin>>t;
+while(t--)
+{
+solve();   
+}
+ 
+} 
