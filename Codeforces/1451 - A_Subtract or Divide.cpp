6343901589751cbe21d041d287ec4c249a798990
Ankit Singh Dungriyal <36338087+ankit-singh-dungriyal@users.noreply.github.com>
2021-02-23 22:18:15 +0530

    A_Subtract or Divide

---
+++ Codeforces/1451 - A_Subtract or Divide.cpp
@@ -0,0 +1,56 @@
+/*
+Contest Id: 1451
+Submission Id: 106440357
+Date & Time: 04/02/2021 15:15:16
+Tags/Concept used: ['greedy', 'math']
+*/
+
+
+#include<bits/stdc++.h>
+// #include<thread>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define pb push_back
+#define mp make_pair
+#define wr(i) cout<<#i<<" = "<<i<<",  ";
+#define wre(i) cout<<#i<<"  =  "<<i<<endl;
+#define all(v) v.begin(),v.end()
+typedef long long ll;
+#define lim 1000000000000000000LL
+typedef unsigned long long ull;
+#define mod 1000000007
+#define M 31
+#define N 30
+#define F first
+#define S second
+using u64= uint64_t;
+
+void solve(){
+    ll n;
+    cin>>n;
+    if(n==1)cout<<0;
+    else if(n==2)cout<<1;
+    else if(n&1){
+        if(n==3)cout<<2;
+        else cout<<3;
+    }
+    else cout<<2;
+    cout<<"\n";
+    return;
+}
+
+int main()
+{
+fast
+#ifndef ONLINE_JUDGE
+freopen("input.txt","r",stdin);
+freopen("output.txt","w",stdout);
+#endif
+ll t=1LL;
+cin>>t;
+while(t--)
+{
+solve();   
+}
+ 
+} 
