a84deb041f301c5eccce37b325095c2d64599dfe
Ankit Singh Dungriyal <36338087+ankit-singh-dungriyal@users.noreply.github.com>
2021-02-23 22:17:01 +0530

    A_Reorder

---
+++ Codeforces/1436 - A_Reorder.cpp
@@ -0,0 +1,22 @@
+/*
+Contest Id: 1436
+Submission Id: 108283659
+Date & Time: 23/02/2021 16:09:16
+Tags/Concept used: ['math']
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main(){
+    ll t;
+    cin>>t;
+    while(t--){
+        ll n,m;
+        cin>>n>>m;
+        ll x,sum=0;
+        for(int i=0;i<n;i++)cin>>x,sum+=x;
+        if(sum==m)cout<<"YES\n";
+        else cout<<"NO\n";
+    }
+}
