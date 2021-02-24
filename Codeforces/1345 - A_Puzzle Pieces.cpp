fcc50ea52ef4839fa044c02e80667bda0260c88e
Ankit Singh Dungriyal <36338087+ankit-singh-dungriyal@users.noreply.github.com>
2021-02-24 20:13:43 +0530

    A_Puzzle Pieces

---
+++ Codeforces/1345 - A_Puzzle Pieces.cpp
@@ -0,0 +1,54 @@
+/*
+Contest Id: 1345
+Submission Id: 108411694
+Date & Time: 24/02/2021 20:13:05
+Tags/Concept used: ['math']
+*/
+
+
+#include<bits/stdc++.h>
+// #include<thread>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define pb push_back
+#define mp make_pair
+#define wr(i) cout<<#i<<" = "<<i<<",  ";
+#define wre(i) cout<<#i<<"  =  "<<i<<endl;
+#define all(v) v.begin(),v.end()
+typedef long long ll;
+#define lim 1000000000000000000LL
+typedef unsigned long long ull;
+#define mod 1000000007
+#define M 31
+#define N 30
+#define F first
+#define S second
+using u64= uint64_t;
+
+
+
+void solve(){
+    ll n,m;
+    cin>>n>>m;
+    if(n==1 || m==1 ||(n==2 && m==2))cout<<"YES\n";
+    else cout<<"NO\n";
+
+    return;
+}
+
+int main()
+{
+fast
+#ifndef ONLINE_JUDGE
+freopen("input.txt","r",stdin);
+freopen("output.txt","w",stdout);
+#endif
+ll t=1LL;
+cin>>t;
+while(t--)
+{
+solve();  
+
+}
+ 
+} 
