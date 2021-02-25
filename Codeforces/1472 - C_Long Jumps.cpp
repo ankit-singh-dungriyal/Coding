2470408876af50536ad5252565609b0b1818f0b4
Ankit Singh Dungriyal <36338087+ankit-singh-dungriyal@users.noreply.github.com>
2021-02-25 20:06:31 +0530

    C_Long Jumps

---
+++ Codeforces/1472 - C_Long Jumps.cpp
@@ -0,0 +1,60 @@
+/*
+Contest Id: 1472
+Submission Id: 108467711
+Date & Time: 25/02/2021 16:21:51
+Tags/Concept used: ['dp', 'graphs']
+*/
+
+
+#include<bits/stdc++.h>
+// #include<thread>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define pb push_back
+#define mp make_pair
+#define wr(i) cout<<#i<<" = "<<i<<",  ";
+#define wre(i) cout<<#i<<"  =  "<<i<<endl;
+#define all(v) v.begin(),v.end()
+typedef long long ll;
+#define lim 1000000000000000000LL
+typedef unsigned long long ull;
+#define mod 1000000007
+#define M 31
+#define N 30
+#define F first
+#define S second
+using u64= uint64_t;
+
+
+
+void solve(){
+    ll n;
+    cin>>n;
+    vector<ll>v(n);
+    for(auto &x:v)cin>>x;
+    for(int i=n-2;i>=0;i--){
+        int j=v[i]+i;
+        if(j<n)
+        v[i]=max(v[i],v[i]+v[j]);
+    }
+    cout<<*max_element(all(v))<<"\n";
+
+    return;
+}
+
+int main()
+{
+fast
+#ifndef ONLINE_JUDGE
+freopen("input.txt","r",stdin);
+freopen("output.txt","w",stdout);
+#endif
+ll t=1LL;
+cin>>t;
+while(t--)
+{
+solve();  
+
+}
+ 
+} 
