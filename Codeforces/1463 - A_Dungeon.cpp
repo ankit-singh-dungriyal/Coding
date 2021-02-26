eaf50c48f67aadd6845dfe8c4fd2fc6190a84ad4
Ankit Singh Dungriyal <36338087+ankit-singh-dungriyal@users.noreply.github.com>
2021-02-26 20:06:38 +0530

    A_Dungeon

---
+++ Codeforces/1463 - A_Dungeon.cpp
@@ -0,0 +1,54 @@
+/*
+Contest Id: 1463
+Submission Id: 108528345
+Date & Time: 26/02/2021 15:28:48
+Tags/Concept used: ['binary search', 'math']
+*/
+
+
+#include<bits/stdc++.h>
+// #include<thread>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define pb push_back
+#define mp make_pair
+#define wr(i) cout<<#i<<" = "<<i<<",  ";
+#define wre(i) cout<<#i<<"  =  "<<i<<endl;
+#define all(v) v.begin(),v.end()
+typedef long long ll;
+#define lim 1000000000000000000LL
+typedef unsigned long long ull;
+#define mod 1000000007
+#define M 31
+#define N 30
+#define F first
+#define S second
+using u64= uint64_t;
+
+
+
+void solve(){
+    ll a,b,c;
+    cin>>a>>b>>c;
+    ll sum=a+b+c;
+    if(sum%9==0 && a>=sum/9 && b>=sum/9 && c>=sum/9)cout<<"Yes\n";
+    else cout<<"No\n";
+    return;
+}
+
+int main()
+{
+fast
+#ifndef ONLINE_JUDGE
+freopen("input.txt","r",stdin);
+freopen("output.txt","w",stdout);
+#endif
+ll t=1LL;
+cin>>t;
+while(t--)
+{
+solve();  
+
+}
+ 
+} 
