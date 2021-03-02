47a458f3122329b4bfdb04235c16101c9a953124
Ankit Singh Dungriyal <36338087+ankit-singh-dungriyal@users.noreply.github.com>
2021-03-02 11:46:42 +0530

    Create Length of Last Word.cpp

---
+++ Leetcode/Easy/Length of Last Word.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int ans=0;
+        int i=s.length()-1;
+        while(i>=0 && s[i]==' ')i--;
+        for(;i>=0;i--){
+            if(s[i]==' ')break;
+            ans++;
+        }
+        return ans;
+    }
+};
