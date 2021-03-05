a24161fd797d2f7e2c7a4eac5ae6a33538706991
Ankit Singh Dungriyal <36338087+ankit-singh-dungriyal@users.noreply.github.com>
2021-03-05 10:12:02 +0530

    Create Populating Next Right Pointers in Each Node II.cpp

---
+++ Leetcode/Medium/Populating Next Right Pointers in Each Node II.cpp
@@ -0,0 +1,40 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if(!root)return root;
+        queue<Node*>q;
+        q.push(root);
+        while(!q.empty()){
+            int n=q.size();
+            Node *tmp;
+            for(int i=0;i<n;i++){
+                tmp=q.front();
+                if(tmp->left)q.push(tmp->left);
+                if(tmp->right)q.push(tmp->right);
+                q.pop();
+                if(i<n-1)tmp->next=q.front();
+                
+            }
+            tmp->next=NULL;
+        }
+        return root;
+    }
+};
