c9e2d4b8a62d4bce9ef5dc7f7bb4174d5b34b9fd
Ankit Singh Dungriyal <36338087+ankit-singh-dungriyal@users.noreply.github.com>
2021-03-09 20:14:59 +0530

    B_Polycarp Training

---
+++ Codeforces/1165 - B_Polycarp Training.cpp
@@ -0,0 +1,61 @@
+/*
+Contest Id: 1165
+Submission Id: 109457040
+Date & Time: 09/03/2021 15:45:00
+Tags/Concept used: ['data structures', 'greedy', 'sortings']
+*/
+
+#include<bits/stdc++.h>
+// #include<thread>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define pb push_back
+#define mp make_pair
+#define wr(i) cout<<#i<<" = "<<i<<",  ";
+#define wre(i) cout<<#i<<"  =  "<<i<<endl;
+#define all(v) v.begin(),v.end()
+typedef long long ll;
+#define lim 1000000000000LL
+typedef unsigned long long ull;
+#define mod 1000000007
+#define M 31
+#define N 30
+#define F first
+#define S second
+using u64= uint64_t;
+
+
+void solve(){
+  ll n;
+  cin>>n;
+  vector<ll>v(n);
+  for(auto &x:v)cin>>x;
+  ll ans=1;
+  sort(all(v));
+  for(int i=0;i<n;i++){
+    if(v[i]>=ans)ans++;
+  }
+  cout<<ans-1<<"\n";
+
+  return;
+
+}
+
+int main()
+{
+fast
+#ifndef ONLINE_JUDGE
+freopen("input.txt","r",stdin);
+freopen("output.txt","w",stdout);
+#endif
+
+ll t=1LL;
+// cin>>t;
+
+while(t--)
+{
+solve();  
+
+}
+ 
+} 
