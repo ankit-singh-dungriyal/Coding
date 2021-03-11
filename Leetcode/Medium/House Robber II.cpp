da0031c8e5771bccf29a26ffe7a2cb385b594687
Ankit Singh Dungriyal <36338087+ankit-singh-dungriyal@users.noreply.github.com>
2021-03-11 12:10:49 +0530

    Create House Robber II.cpp

---
+++ Leetcode/Medium/House Robber II.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1)return nums[0];
+        if(n==2)return max(nums[0],nums[1]);
+        vector<int>dp1(n-1);
+        dp1[0]=nums[0];
+        dp1[1]=max(nums[0],nums[1]);
+        for(int i=2;i<n-1;i++)dp1[i]=max(dp1[i-2]+nums[i],dp1[i-1]);
+        reverse(nums.begin(),nums.end());
+        int ans=dp1[n-2];
+        dp1[0]=nums[0];
+        dp1[1]=max(nums[0],nums[1]);
+        for(int i=2;i<n-1;i++)dp1[i]=max(dp1[i-2]+nums[i],dp1[i-1]);
+        ans=max(ans,dp1[n-2]);
+        return ans;
+    }
+};
