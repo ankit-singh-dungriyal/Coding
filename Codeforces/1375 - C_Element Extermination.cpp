5c4fdb4c9cce10edf92ce62080eaa8e6fddcb276
Ankit Singh Dungriyal <36338087+ankit-singh-dungriyal@users.noreply.github.com>
2021-03-13 22:18:26 +0530

    C_Element Extermination

---
+++ Codeforces/1375 - C_Element Extermination.cpp
@@ -0,0 +1,58 @@
+/*
+Contest Id: 1375
+Submission Id: 109909612
+Date & Time: 13/03/2021 21:45:29
+Tags/Concept used: ['constructive algorithms', 'data structures', 'greedy']
+*/
+
+#include<bits/stdc++.h>
+// #include<thread>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define pb push_back
+#define mp make_pair
+#define wr(i) cout<<#i<<" = "<<i<<",  ";
+#define wre(i) cout<<#i<<"  =  "<<i<<endl;
+#define all(v) v.begin(),v.end()
+typedef long long ll;
+#define lim 1000000000000LL
+typedef unsigned long long ull;
+#define mod 1000000007
+#define M 31
+#define N 10000007
+#define F first
+#define S second
+using u64= uint64_t;
+
+
+
+
+void solve(){
+  ll n;
+  cin>>n;
+  vector<ll>v(n);
+  for(auto &x:v)cin>>x;
+  cout<<(v[0]<v[n-1]?"YES\n":"NO\n");
+  
+  return;
+
+}
+
+int main()
+{
+fast
+#ifndef ONLINE_JUDGE
+freopen("input.txt","r",stdin);
+freopen("output.txt","w",stdout);
+#endif
+
+ll t=1LL;
+cin>>t;
+
+while(t--)
+{
+solve();  
+
+}
+ 
+} 
