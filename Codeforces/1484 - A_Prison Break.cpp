74d9c8d07bcb889572dd4ef0182f6515a93c7e3c
Ankit Singh Dungriyal <36338087+ankit-singh-dungriyal@users.noreply.github.com>
2021-03-22 15:01:40 +0530

    A_Prison Break

---
+++ Codeforces/1484 - A_Prison Break.cpp
@@ -0,0 +1,55 @@
+/*
+Contest Id: 1484
+Submission Id: 110627920
+Date & Time: 21/03/2021 18:54:23
+Tags/Concept used: []
+*/
+
+#include<bits/stdc++.h>
+// #include<thread>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define pb push_back
+#define mp make_pair
+#define wr(i) cout<<#i<<" = "<<i<<",  ";
+#define wre(i) cout<<#i<<"  =  "<<i<<endl;
+#define all(v) v.begin(),v.end()
+typedef long long ll;
+#define lim 1000000000000LL
+typedef unsigned long long ull;
+#define mod 1000000007
+#define MAX 400005
+#define N 10000007
+#define F first
+#define S second
+using u64= uint64_t;
+
+
+
+void solve(){
+  ll a,b;
+  cin>>a>>b;
+  cout<<a*b<<"\n";
+  
+  return;
+
+}
+
+int main()
+{
+fast
+#ifndef ONLINE_JUDGE
+freopen("input.txt","r",stdin);
+freopen("output.txt","w",stdout);
+#endif
+
+ll t=1LL;
+cin>>t;
+
+while(t--)
+{
+solve();  
+
+}
+ 
+} 
