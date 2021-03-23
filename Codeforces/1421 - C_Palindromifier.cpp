133be84ff057f1341240c31dee479488fb164580
Ankit Singh Dungriyal <36338087+ankit-singh-dungriyal@users.noreply.github.com>
2021-03-23 21:19:12 +0530

    C_Palindromifier

---
+++ Codeforces/1421 - C_Palindromifier.cpp
@@ -0,0 +1,56 @@
+/*
+Contest Id: 1421
+Submission Id: 110827760
+Date & Time: 23/03/2021 21:18:49
+Tags/Concept used: ['constructive algorithms', 'strings']
+*/
+
+#include<bits/stdc++.h>
+// #include<thread>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define pb push_back
+#define mp make_pair
+#define wr(i) cout<<#i<<" = "<<i<<",  ";
+#define wre(i) cout<<#i<<"  =  "<<i<<endl;
+#define all(v) v.begin(),v.end()
+typedef long long ll;
+#define lim 1000000000000LL
+typedef unsigned long long ull;
+#define mod 1000000007
+#define MAX 400005
+#define N 10000007
+#define F first
+#define S second
+using u64= uint64_t;
+
+
+
+
+void solve(){
+  string s;
+  cin>>s;
+  ll n=s.length();
+  cout<<3<<"\nR "<<n-1<<"\nL "<<n<<"\nL "<<2<<"\n";
+  return;
+
+}
+
+int main()
+{
+fast
+#ifndef ONLINE_JUDGE
+freopen("input.txt","r",stdin);
+freopen("output.txt","w",stdout);
+#endif
+
+ll t=1LL;
+// cin>>t;
+
+while(t--)
+{
+solve();  
+
+}
+ 
+} 
