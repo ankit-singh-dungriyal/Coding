e7d60326407340e5f76d327404114d89551dca93
Ankit Singh Dungriyal <36338087+ankit-singh-dungriyal@users.noreply.github.com>
2021-03-23 15:52:32 +0530

    Create Equal elements.cpp

---
+++ Hackerearth/March Circuits 2021/Equal elements.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+// #include<thread>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define pb push_back
+#define mp make_pair
+#define wr(i) cout<<#i<<" = "<<i<<",  ";
+#define wre(i) cout<<#i<<"  =  "<<i<<endl;
+#define all(v) v.begin(),v.end()
+typedef long long ll;
+#define lim 1000000000000LL
+typedef unsigned long long ull;
+#define mod 1000000007
+#define MAX 400005
+#define N 10000007
+#define F first
+#define S second
+using u64= uint64_t;
+
+
+
+void solve(){
+  ll n;
+  cin>>n;
+  ll a=0,b=0,x;
+  for(int i=0;i<n;i++){
+    cin>>x;
+    if(x&1)a++;
+    else b++;
+  }
+  cout<<min(a,b)<<"\n";
+  return;
+
+}
+
+int main()
+{
+fast
+#ifndef ONLINE_JUDGE
+freopen("input.txt","r",stdin);
+freopen("output.txt","w",stdout);
+#endif
+
+ll t=1LL;
+cin>>t;
+
+while(t--)
+{
+solve();  
+
+}
+ 
+} 
