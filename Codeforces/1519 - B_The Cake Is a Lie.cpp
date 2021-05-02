af7456ba55a67f092c4cd5bb4e36351d22cea8fe
Ankit Singh Dungriyal <36338087+ankit-singh-dungriyal@users.noreply.github.com>
2021-05-02 10:34:45 +0530

    B_The Cake Is a Lie

---
+++ Codeforces/1519 - B_The Cake Is a Lie.cpp
@@ -0,0 +1,54 @@
+/*
+Contest Id: 1519
+Submission Id: 114819038
+Date & Time: 02/05/2021 09:10:35
+Tags/Concept used: ['dp', 'math']
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define pb push_back
+#define mp make_pair
+#define wr(i) cout<<#i<<" = "<<i<<",  ";
+#define wre(i) cout<<#i<<"  =  "<<i<<endl;
+#define all(v) v.begin(),v.end()
+typedef long long ll;
+#define lim 1000000000000000000LL
+typedef unsigned long long ull;
+#define mod 1000000007
+#define MAX 400005
+#define N 10000007
+#define F first
+#define S second
+using u64= uint64_t;
+
+
+void solve(){
+    ll x,y,k;
+    cin>>x>>y>>k;
+    ll d=(x-1)+(y-1)*x;
+    if(d==k)cout<<"YES\n";
+    else cout<<"NO\n";
+    
+
+
+}
+
+int main()
+{
+fast
+#ifndef ONLINE_JUDGE
+freopen("input.txt","r",stdin);
+freopen("output.txt","w",stdout);
+#endif
+
+ll t=1LL;
+cin>>t;
+while(t--)
+{
+solve();  
+
+}
+ 
+} 
